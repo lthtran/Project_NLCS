c135ed53c64f8b0bb0a92ae9d4be08e881aba0fe
Le Thi Huyen Tran B2014710 <tranb2014710@student.ctu.edu.vn>
2023-10-29 17:25:12 +0700

    order

--- order.php
+++ order.php
@@ -25,13 +25,13 @@ while($row_cart = mysqli_fetch_array($run_cart)){
 
     while($row_products = mysqli_fetch_array($run_products)){
         $sub_total = $row_products['product_price']*$pro_qty;
-        $insert_customer_order = "insert into customer_orders (customer_id,due_amount,invoice_no,qty,size,order_date,order_status) values 
-        ('$customer_id','$sub_total','$invoice_no','$pro_qty','$pro_size',NOW(),'$status')";
+        $insert_customer_order = "insert into customer_orders (customer_id,due_amount,invoice_no,qty,order_date,order_status) values 
+        ('$customer_id','$sub_total','$invoice_no','$pro_qty',NOW(),'$status')";
 
         $run_customer_order = mysqli_query($con,$insert_customer_order);
 
-        $insert_pending_order = "insert into pending_orders(customer_id,invoice_no,product_id,qty,size,order_status) values 
-        ('$customer_id','$invoice_no','$pro_id','$pro_qty','$pro_size','$status')";
+        $insert_pending_order = "insert into pending_orders(customer_id,invoice_no,product_id,qty,order_status) values 
+        ('$customer_id','$invoice_no','$pro_id','$pro_qty','$status')";
 
         $run_pending_order = mysqli_query($con,$insert_pending_order);
         $delete_cart = "delete from cart where ip_add='$ip_add'";
