27bb11c2447c8d9a70cf98bc743d14f942e4186d
Le Thi Huyen Tran B2014710 <tranb2014710@student.ctu.edu.vn>
2023-10-29 17:26:43 +0700

    view_customer

--- admin_area/view_customers.php
+++ admin_area/view_customers.php
@@ -2,9 +2,18 @@
     if(!isset($_SESSION['admin_email'])){
         echo"<script>window.open('login.php','_self')</script>";
     }
-    else{
+    else{    
 ?>
 
+<div class="row">
+    <div class="col-lg-12">
+        <ol class="breadcrumb">
+            <li class="active">
+                <i class="fa fa-dashboard"></i> Bảng điều khiển/ Xem khách hàng
+            </li>
+        </ol>
+    </div>
+</div>
 
 <div class="row">
     <div class="col-lg-12">
@@ -14,32 +23,61 @@
                     <i class="fa fa-tags"></i> Xem khách hàng
                 </h3>
             </div>
-            <div class="col-md-4">
-                <div class="panel">
-                    <div class="panel-body">
-                        <div class="mb-md thumb-info">
-                            <img src="admin_images/<?php echo $admin_image ;?>" alt="<?php echo $admin_image ;?>" class="rounded img-responsive">
-                            <div class="thumb-info-title">
-                                <span class="thumb-info-inner"> <?php echo $admin_name ;?> </span>
-                            </div>
-                        </div>
-                        <div class="mb-md">
-                            <div class="widget-content-expanded">
-                                <i class="fa fa-user"></i> <span> Email: </span> <?php echo $admin_email ;?> <br/>
-                                <i class="fa fa-envelope"></i> <span> Liên hệ </span> <?php echo $admin_contact ;?> <br/>
-                            </div>
-                            <hr class="dotted short">
-                            <h5 class="text-muted"> About me </h5>
-                            <p>
-                                <?php echo $admin_about; ?>
-                            </p>
-                        </div>
-                    </div>
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover">
+                        <thread>
+                            <tr class="text-center">
+                                <th> STT: </th>
+                                <th> Tên khách hàng: </th>
+                                <th> Ảnh: </th>
+                                <th> Email: </th>
+                                <th> Quốc gia: </th>
+                                <th> Thành phố: </th>
+                                <th> Địa chỉ: </th>
+                                <th> Liên hệ: </th>
+                                <th> Xóa </th>
+                            </tr>
+                        </thread>
+                        <tbody>
+                            <?php
+                                $i=0;
+                                $get_c = "select * from customers";
+                                $run_c = mysqli_query($con, $get_c);
+                                while($row_c=mysqli_fetch_array($run_c)){
+                                    $c_id = $row_c['customer_id'];
+                                    $c_name = $row_c['customer_name'];
+                                    $c_img = $row_c['customer_image'];
+                                    $c_email = $row_c['customer_email'];
+                                    $c_country = $row_c['customer_country'];
+                                    $c_city = $row_c['customer_city'];
+                                    $c_address = $row_c['customer_address'];
+                                    $c_contact = $row_c['customer_contact'];
+
+                                    $i++;
+
+                            ?>
+                            <tr class="text-center">
+                                <td> <?php echo $i; ?></td>
+                                <td> <?php echo $c_name; ?></td>
+                                <td> <img src="../customer/customer_images/<?php echo $c_img; ?>" width="70" height="60"></td>
+                                <td> <?php echo $c_email; ?></td>
+                                <td> <?php echo $c_country; ?></td>
+                                <td> <?php echo $c_city; ?></td>
+                                <td> <?php echo $c_address; ?></td>
+                                <td> <?php echo $c_contact; ?></td>
+                                <td> <a href="index.php?delete_customer=<?php echo $c_id; ?>">
+                                        <i class="fa fa-trash-o"></i> Xóa 
+                                    </a>
+                                </td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-
         </div>
     </div>
 </div>
-    
+
 <?php } ?>
