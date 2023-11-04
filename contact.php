e4cef4e2d25d627b0546c2967919447c9fe14dd1
Le Thi Huyen Tran B2014710 <tranb2014710@student.ctu.edu.vn>
2023-11-04 18:51:28 +0700

    contact

--- contact.php
+++ contact.php
@@ -8,10 +8,10 @@
             <div class="col-md-12">
                 <ul class="breadcrumb">
                     <li>
-                        <a href="index.php">Home</a>
+                        <a href="index.php">Trang chủ</a>
                     </li>
                     <li>
-                        Contact Us
+                        Liên hệ
                     </li>
                 </ul>
             </div>
@@ -27,14 +27,14 @@
                 <div class="box">
                     <div class="box-header">
                         <center>
-                            <h2> Feel free to Contact Us </h2>
+                            <h2> Biểu mẫu liên hệ </h2>
                             <p class="text-muted">
-                                If you have any problems, feel free to contact us. Our Customer Service work <strong>24/7</strong>
+                                Nếu bạn gặp vấn đề, liên hệ ngay. Cửa hàng của chúng tớ làm việc <strong>24/7</strong>
                             </p>
                         </center>
-                        <form action="contact.php" method="post">
+                        <form action="send.php" method="post">
                             <div class="form-group">
-                                <label>Name</label>
+                                <label>Họ tên</label>
                                 <input type="text" class="form-control" name="name" required>
                             </div>
 
@@ -44,18 +44,18 @@
                             </div>
 
                             <div class="form-group">
-                                <label>Subject</label>
+                                <label>Mô tả vấn đề</label>
                                 <input type="text" class="form-control" name="subject" required>
                             </div>
 
                             <div class="form-group">
-                                <label>Message</label>
-                                <textarea name="message" class="form-control"></textarea>
+                                <label>Lời nhắn</label>
+                                <textarea name="message" rows="5" class="form-control"></textarea>
                             </div>
 
                             <div class="text-center">
-                                <button type="submit" name="submit" class="btn btn-primary">
-                                <i class="fa fa-user-md"></i> Send Message
+                                <button data-toggle="modal" data-target="#myModal" type="submit" name="submit" class="btn btn-primary btn-lg btn-block">
+                                <i class="fa fa-user-md"></i> Gửi 
                                 </button>
                             </div>
 
@@ -67,15 +67,30 @@
                                $sender_email = $_POST['email'];
                                $sender_subject = $_POST['subject'];
                                $sender_message = $_POST['message'];
-                               $receiver_email= "mugianto4th@gmail.com";
+                               $receiver_email= "huyentrantb66666@gmail.com";
                                mail($receiver_email,$sender_name,$sender_subject,$sender_message,$sender_email);
 
                                $email = $_POST['email'];
                                $subject = "Welcome to my website";
                                $msg = "Thanks for sending us message. ASAP we will reply your message";
-                               $from = "mugianto4th@gmail.com"; 
+                               $from = "tranb2014710@student.ctu.edu.vn"; 
                                mail($email, $subject,$msg,$from);
-                               echo "<h2 align='center'> Your message has sent successfully </h2>";
+                               echo "<div class='modal fade' id='myModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>
+                               <div class='modal-dialog' role='document'>
+                                 <div class='modal-content'>
+                                   <div class='modal-header'>
+                                     <button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+                                     <h4 class='modal-title' id='myModalLabel'>Phản hồi</h4>
+                                   </div>
+                                   <div class='modal-body'>
+                                     Yêu cầu đã được gửi đi!!
+                                   </div>
+                                   <div class='modal-footer'>
+                                     <button type='button' class='btn btn-default' data-dismiss='modal'>Đóng</button>
+                                   </div>
+                                 </div>
+                               </div>
+                             </div>";
                             }
                         ?>
 
