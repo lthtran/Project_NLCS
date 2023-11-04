226d99f201af3882fd60d9090de21ddb96486f37
Le Thi Huyen Tran B2014710 <tranb2014710@student.ctu.edu.vn>
2023-11-04 18:52:44 +0700

    delete

--- admin_area/delete_product.php
+++ admin_area/delete_product.php
@@ -8,7 +8,7 @@
 <?php
     if(isset($_GET['delete_product'])){
         $delete_id= $_GET['delete_product'];
-        $delete_pro= "delete * from products where product_id='$delete_id'";
+        $delete_pro= "delete from `products` where product_id='$delete_id'";
         $run_delete = mysqli_query($con,$delete_pro);
         if($run_delete){
             echo "<script>alert('Sản phẩm đã được xóa. ')</script>";
