faa28921b1b87c217017ae3c69ac9a61894d30c6
Le Thi Huyen Tran B2014710 <tranb2014710@student.ctu.edu.vn>
2023-11-10 21:34:17 +0700

    sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,6 +1,6 @@
 <div class="panel panel-default sidebar-menu">
     <div class="panel-heading">
-        <h3 class="panel-title">Danh mục sản phẩm</h3>
+        <h3 class="panel-title">Loại sản phẩm</h3>
     </div>
     <div class="panel-body">
         <ul 
